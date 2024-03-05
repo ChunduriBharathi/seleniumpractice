6ac1757fdcf91baf97c6750bc5dd9af387bb8892
ChunduriBharathi <bharathchunduri88@gmail.com>
2024-03-05 06:37:59 +0530

    Adding print

--- src/test/java/seleniumnote/homepageTest.java
+++ src/test/java/seleniumnote/homepageTest.java
@@ -5,6 +5,7 @@ public class homepageTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//this is Bharathi
+	System.out.println("This is bharathi from 3i");
 
 	}
 
